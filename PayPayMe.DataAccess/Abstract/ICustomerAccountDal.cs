9e58fe83a36675f4c9ffe1cbf3e4598d207a98bb
abdullahdnz <aabdullah.dnz@gmail.com>
2023-06-05 13:03:56 +0300

    Interface and repositories was added.

---
+++ PayPayMe.DataAccess/Abstract/ICustomerAccountDal.cs
@@ -0,0 +1,13 @@
+﻿using PayPayMe.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayPayMe.DataAccess.Abstract
+{
+    public interface ICustomerAccountDal : IGenericDal<CustomerAccount>
+    {
+    }
+}
---
+++ PayPayMe.DataAccess/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using PayPayMe.DataAccess.Abstract;
+using PayPayMe.DataAccess.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayPayMe.DataAccess.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public void Delete(T entity)
+        {
+            using var context = new AppDbContext();
+            context.Set<T>().Remove(entity);
+            context.SaveChanges();
+        }
+
+        public T GetByID(int id)
+        {
+            using var context = new AppDbContext();
+            return context.Set<T>().Find(id);
+        }
+
+        public List<T> GetList()
+        {
+            using var context = new AppDbContext();
+            return context.Set<T>().ToList();
+        }
+
+        public void Insert(T entity)
+        {
+            using var context = new AppDbContext();
+            context.Set<T>().Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            using var context = new AppDbContext();
+            context.Set<T>().Update(entity);
+            context.SaveChanges();
+        }
+    }
+}
